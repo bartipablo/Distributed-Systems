b371aa3e62c0f6f0c335b5bf7d84e4137c394f1c
bartipablo <bartek.pawlowski00@gmail.com>
2024-03-05 21:57:24 +0100

    init lab02

---
+++ 02-RestAPI/RestDIstributedSystem/src/main/java/com/bartipablo/restdistributedsystem/RestDistributedSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bartipablo.restdistributedsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDistributedSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestDistributedSystemApplication.class, args);
+    }
+
+}
---
+++ 02-RestAPI/RestDIstributedSystem/src/test/java/com/bartipablo/restdistributedsystem/RestDistributedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartipablo.restdistributedsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDistributedSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
