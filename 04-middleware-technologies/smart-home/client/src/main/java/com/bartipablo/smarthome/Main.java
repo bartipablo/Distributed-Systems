352aa41031abc8b83f46812fc6cbea82fda130f3
bartipablo <bartek.pawlowski00@gmail.com>
2024-06-16 02:23:05 +0200

    init java client grpc

---
+++ 04-middleware-technologies/smart-home/client/src/main/java/com/bartipablo/smarthome/Main.java
@@ -0,0 +1,9 @@
+package com.bartipablo.smarthome;
+
+import com.smarthome.bartipablo.grpc.AddRequest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
